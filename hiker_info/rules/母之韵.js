88c9a15ad47216b6d5d1e2476c40f98f5d689709
ssmq-5201314 <ssmq5201314@gmail.com>
2026-04-01 15:12:02 +0800

    Update 母之韵.js

--- hiker_info/rules/母之韵.js
+++ hiker_info/rules/母之韵.js
@@ -202,6 +202,7 @@ const csdown = {
         try {
             if (!getMyVar('info', '')) {
                 let url = getItem('host') + '/api/v1/member/info?t=' + t;
+                log(url)
                 let body = '{"timestamp":' + t + ',"version":"0.1.0","os":"h5","uid":' + getItem('uid') + ',"token":"' + getItem('token') + '"}';
                 let data = post(url, body);
                 let uid = data.uid + '';
